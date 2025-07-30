bce9c247bcde91059aed845d285d96b7036dbd8f
Jonas Rosa <jonasdavidrosajf@gmail.com>
2025-07-29 23:10:53 -0300

    ajuste

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: {
+    proxy: {
+      "/v1/api": "http://157.245.118.106:8080",
+    },
+  },
 });
